b1cd35d2f37024911a404c8ee1d4d452042856c1
ekaterina <whitedevil93@mail.ru>
2023-03-05 13:30:06 +0300

    массив

---
+++ контрольная/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.Clear();
+Console.Write("Наш массив: ");
+string[] arrayA = new string[] { "123", "?", "Moscow", "56*@", "OMG", "Rus", ":-((", ":-0" };
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+void Formation(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        string numbers = arr[i];
+        if (numbers.Length <= 3)
+        {
+            arr[i] = numbers;
+    }
+        else arr[i] = string.Empty;
+    }
+}
+PrintArray(arrayA);
+Formation(arrayA);
+Console.Write("Новый массив: ");
+PrintArray(arrayA);
